09698b73590bd716c6d3a08ea2ee73e9466f86e3
Yuhui <yuhui@users.noreply.github.com>
2024-01-29 22:34:31 +0800

    feat: add support for replacer and space parameters

--- src/lib/dataElements/stringify.js
+++ src/lib/dataElements/stringify.js
@@ -28,11 +28,14 @@ const validateValue = require('../helpers/validateValue');
  * Convert a JavaScript object or value to a JSON string.
  *
  * @param {Object} settings The data element settings object.
- * @param {String} settings.objectValue Object to convert to a JSON string.
+ * @param {Object} settings.objectValue Object to convert to a JSON string.
+ * @param {Function} [settings.replacer] (optional) Function or array to alter the behaviour of the
+ *    stringification process.
+ * @param {String or Number} [settings.space] (optional) String or number to insert white space.
  *
  * @returns {String} JSON string representing the given object.
  */
-module.exports = function(settings) {
+module.exports = function({ objectValue, replacer = null, space = null } = {}) {
   try {
     validateValue(objectValue);
   } catch (e) {
@@ -42,11 +45,42 @@ module.exports = function(settings) {
     );
     return;
   }
+  if (replacer) {
+    try {
+      validateValue(replacer, ['array', 'function']);
+    } catch (e) {
+      logError(`Stringify: replace ${e.message}`, replacer);
+      return;
+    }
 
+    // an array replacer must contain strings or numbers only
+    if (Object.prototype.toString.call(replacer) === '[object Array]') {
+      let isReplacerValidArray;
+      try {
+        isReplacerValidArray = replacer.every((item) => validateValue(item, ['number', 'string']));
+      } catch (e) {
+        isReplacerValidArray = false;
+      }
+      if (!isReplacerValidArray) {
+        logError('Stringify: replacer items must be strings or numbers', replacer);
+        return;
+      }
+    }
+  }
+  if (space) {
+    try {
+      validateValue(space, ['string', 'number']);
+    } catch (e) {
+      logError(`Stringify: space ${e.message}`, space);
+      return;
+    }
+  }
 
-  var returnValue;
+  const { stringify } = JSON;
+
+  let returnValue;
   try {
-    returnValue = JSON.stringify(objectValue);
+    returnValue = stringify(objectValue, replacer, space);
   } catch (e) {
     logError('Stringify: unexpected error occurred', e);
     return;
